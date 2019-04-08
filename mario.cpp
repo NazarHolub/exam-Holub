834bcd847eb71f5e3d5550d8b437bf3cbe45cf9b
anonymous <user@step.itstep.rv.ua>
2019-04-08 19:08:24 +0300

    added functions Pyramid and Pyramid2 qw wq ::qw ::wq
    ::wq
    ::qw
    ::wq
    ::q
    
    e enter the commit message for your changes. Lines starting

--- mario.cpp
+++ mario.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void Pyramid2(char* pointer, int num);
+void Pyramid2(char* pointer, int num);//prototype of function
 void Pyramid(char* pointer, int num);
 int main()
 {
@@ -10,16 +10,38 @@ int main()
 	{
 		cout << "Enter num: ";
 		cin >> num;
-	} while (num < 1);
+		if (!cin)
+		{
+			cin.clear();
+			cout << "enter again:\n ";
+			while (cin.get() != '\n') //checking a number by correctly
+				continue;
+
+			continue;
+		}
+
+	} while (num <= 0);
 	do
 	{
 		cout << "Enter word 1 or 0: ";
 		cin >> choise;
-	} while (choise != 0 && choise !=1);
+		if (!cin)
+		{
+			cin.clear();
+			cout << "enter again: ";  //checking choise
+			while (cin.get() != '\n')
+				continue;
+
+			choise = 10;
+			continue;
+		}
+	} while (choise!=0 && choise!=1);
+
 	if (choise == 1)
 		Pyramid(word, num);
 	else
 		Pyramid2(word, num);
+
 	system("pause");
 	return 0;
 }
@@ -30,11 +52,11 @@ void Pyramid2(char * pointer, int num)
 		for (int k = num - i; k > 0; k--)
 			cout << " ";
 		for (int j = i; j > 0; j--)
-			cout << "*";
+			cout << "#";            //function draw a pyramide
 
 		cout << " ";
 		for (int j = i; j > 0; j--)
-			cout << "*";
+			cout << "#";
 		cout << endl;
 	}
 }
@@ -44,8 +66,8 @@ void Pyramid(char* pointer, int num)
 		{
 			for (int k = num - i; k > 0; k--)
 				cout << " ";
-			for (int j = i; j > 0; j--)
-				cout << "*";
+			for (int j = i; j > 0; j--)     //function drawin a half pyramide
+				cout << "#";
 			cout << endl;
 		}
 }
